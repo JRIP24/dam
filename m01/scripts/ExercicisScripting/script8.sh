522cb22afc7350553c2598bf8015ae709f3699d3
Ronald Intriago <ronaldintriago52@gmail.com>
2019-11-26 16:22:00 +0000

    Update script8.sh

---
+++ m01/scripts/ExercicisScripting/script8.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for i in {1..10};do
+	
+	
+	echo "$i * 1 =  $((1 * $i))"
+	echo "$i * 2 =  $((2 * $i))"
+	echo "$i * 3 =  $((3 * $i))"
+	echo "$i * 4 =  $((4 * $i))"
+	echo "$i * 5 =  $((5 * $i))"
+	echo "$i * 6 =  $((6 * $i))"
+	echo "$i * 7 =  $((7 * $i))"
+	echo "$i * 8 =  $((8 * $i))"
+	echo "$i * 9 =  $((9 * $i))"
+	echo -e "$i * 10 =  $((10 * $i)) \n"
+		
+done
+echo ""
