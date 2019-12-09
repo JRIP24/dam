dd3efc4493e49bb577f513c2d84c8e41c70a0f54
iam2671090 <iam2671090@j05.informatica.escoladeltreball.org>
2019-12-09 20:08:51 +0100

    update 2019/12/09-20:08

--- m01/scripts/ExercicisExpressionsRegulars1/exercici5.sh
+++ m01/scripts/ExercicisExpressionsRegulars1/exercici5.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-cat copia_etc_passwd.txt | egrep "[a-z]:x:1000" | cut -d ":" -f 5 | sed 's/guest/usuari local/'
+#cat copia_etc_passwd.txt | egrep "[a-z]:x:1000" | cut -d ":" -f 5 | sed 's/guest/usuari local/'
+
+sed 's;\([^:]*\):x:1000;\1usuari local;' copia_etc_passwd.txt
+
+#cat copia_etc_passwd.txt | egrep "[a-z]:x:1000" | awk -F :  '$5==gueckos' | sed 's/$gueckos/usuari local/2'
