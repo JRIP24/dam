1707c5cb58987d620fc3c19abb216a521b2cd348
iam2671090 <iam2671090@j05.informatica.escoladeltreball.org>
2020-01-23 21:09:54 +0100

    update 2020/01/23-21:09

---
+++ m02/sql/exercicis_simples_SQL/RonaldIntriagoTraining2.sql
@@ -0,0 +1,59 @@
+--1. Mostrar la fecha de los pedidos con la descripción, cantidad, precio e importe de los artículos de dichos pedidos añadiendo además  el nombre del representante que tomó el pedido para los pedidos cuyo importe del pedido sea superior a 25.000.
+
+
+
+--2. Mostrar un listado de los representantes con su jefe, es decir,  código y nombre de representante y lo mismo para su jefe.
+
+
+
+--3. Mostrar todos los representantes (incluyendo los que no tienen asignada una oficina). Nombre del representante, puesto, ventas, código de oficina y ciudad.
+
+
+
+--4. Como la 2 pero mostrando todos los representantes.
+
+
+
+--5. Obtener una lista de los pedidos con importes superiores a 150 euros, mostrando el código del pedido, el importe, el nombre del cliente que lo solicitó, el nombre del representante que contactó con él por primera vez y la ciudad de la oficina donde el representante trabaja.
+
+
+
+--6. Lista los pedidos tomados durante el mes de octubre del año 1989 (any modificat perquè surti algun resultat) , mostrando solamente el número del pedido, su importe, el nombre del cliente que lo realizó, la fecha y la descripción del producto solicitado.
+
+
+
+--7. Obtener una lista de todos los pedidos tomados por representantes de oficinas de la región Este, mostrando solamente el número del pedido, la descripción del producto, nombre del representante que lo tomó y la región de su oficina. (Afegeixo al select el camp region perquè es vegi be i ordeno per representant).
+
+
+
+--8. Obtener una lista con parejas de representantes y oficinas en donde la cuota del representante es mayor o igual que el objetivo de la oficina, sea o no la oficina en la que trabaja. Mostrar Nombre del representante, cuota del mismo, Ciudad de la oficina, objetivo de la misma.
+
+
+
+--9. Muestra el nombre, las ventas y la ciudad de la oficina de cada representante de la empresa (cal tenir en compte que hi ha algun representant que no té oficina)
+
+
+
+--10. Obtener una lista de la descripción de los productos para los que existe algún pedido en el que se solicita una cantidad mayor a las existencias de dicho producto.
+
+
+
+--11. Lista los nombres de los representantes que tienen una cuota superior a la de su director de oficina. (Mostrar nombre representante, cuota, nombre director y cuota del mismo)
+
+
+
+--12. Obtener una lista de los representantes que trabajan en una oficina distinta de la oficina en la que trabaja su JEFE (enunciat modificat perquè sigui algo diferent a l’anterior), mostrando también el nombre del director y el código de la oficina donde trabaja cada uno de ellos.
+
+
+
+--13. El mismo ejercicio anterior, mostrando en lugar de códigos de oficinas, ciudad del representante y su jefe (exercici avançat).
+
+
+
+--14. Mostrar todos los datos de los representantes que son jefe (evitando repeticiones).
+
+
+
+--15. Obtener los pedidos tomados en los mismos días en que un nuevo representante fue contratado. Mostrar número de pedido, importe, fecha pedido (y fecha de contrato)
+
+
