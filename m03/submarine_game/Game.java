4a44ed15666e85e40442d71097e2193d1b7c208d
Ronald Intriago <ronaldintriago52@gmail.com>
2020-03-10 15:34:39 +0000

    Update m03/submarine_game/Game.java, m03/submarine_game/Menu.java files

---
+++ m03/submarine_game/Game.java
@@ -0,0 +1,32 @@
+import java.utils.Scanner;
+
+package submarine_game;
+
+public class Game{
+
+    public static void main(String[] args){
+
+        Sea mar = new Sea(5,5);
+        System.out.println("Dimenssión de mar: " + mar.sizeX + "x" + mar.sizeY);
+
+        //Creación de submarino
+        Submarine s = new Submarine();
+        // s.cordX = random
+        // s.cordY = random
+
+        //Valor random entre n1 y n2 (n1 < n2), ambos incluidos
+        //Math.floor(Math.random()*(n2-n1+1)+n1));
+        
+        Player p = new Player("Ronald");
+        
+        System.out.println("Hola " + p.name);
+        
+        int misiles = 6;
+
+        p.cargas = misiles;
+
+        System.out.println("Tienes " + p.cargas + " cargas");
+
+
+    }
+}
