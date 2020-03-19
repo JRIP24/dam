eed18f908a2d6761b1526ca2216b07a86b680495
Ronald Intriago <ronaldintriago52@gmail.com>
2020-03-19 17:31:32 +0100

    update 2020/03/19-17:31

--- m03/ClassesObjectesIntriagoRonald/ring/Ring.java
+++ m03/ClassesObjectesIntriagoRonald/ring/Ring.java
@@ -6,7 +6,22 @@ public class Ring{
     String model;
     double ditD;
     double anellD;
-    
+
+    public Ring(String m, double D, double d ){
+        this.model = m;
+        this.ditD = D;
+        this.anellD  = d;
+    }
+
+    public double peso(){
+
+        double ditR = this.ditD/2;
+        double anellR = this.anellD/2;
+        
+        //Peso en mm²
+        return 2 * Math.pow(Math.PI, 2) * ditR * Math.pow(anellR, 2);
+    }
+
 
 
 }
