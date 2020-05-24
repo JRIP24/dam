258fccc9951d1a8e865c1e010ce7c842f4c42845
Ronald Intriago <ronaldintriago52@gmail.com>
2020-05-24 13:26:40 +0200

    update 2020/05/24-13:26

--- m03/archivos/practica1/crud/Crud.java
+++ m03/archivos/practica1/crud/Crud.java
@@ -1,3 +1,4 @@
+
 public class Crud{
 
     private String masterFile;
@@ -38,6 +39,7 @@ public class Crud{
 
         //Esribimos la primera líena
         escritor.println(linea1);
+        
 
         while (line != null){
 
@@ -55,12 +57,14 @@ public class Crud{
                 if (id.compareTo(campos1[0]) > 0 && id.compareTo(campos2[0]) < 0){
                     //System.out.println(id + " va entre " + campos1[0] + " y " + campos2[0]);
                     escritor.println(line);
+                    
                     insertado = true;
 
                 }
 
                 //Escribimos el resto de datos
                 escritor.println(linea2);
+                
             
                 linea1 = linea2;
                 linea2 = lector.readLine();
@@ -70,31 +74,165 @@ public class Crud{
                     //Si llega al final
                     if (id.compareTo(campos2[0]) > 0 && insertado == false){
                         escritor.println(line);
+                        
                     }
                 }
 
 
             }
-
-            escritor.close();
             
             line = lectorCreate.readLine();
         }
 
+        escritor.close();
         lector.close();
-
         lectorCreate.close();
+
+        //Eliminamos master.dat
+        Files.delete(this.masterFile);
+
+        //Renombramos el archivo temporal
+        Files.rename("temporalFile.dat", "master.dat");
+        this.masterFile = "master.dat";
     }
 
     public void delete(){
 
-        Reader lector = new Reader("temporalFile.dat");
-        
+        Reader lectorDelete = new Reader("delete.dat");
+        Reader lector = new Reader(this.masterFile);
+        Writer escritor = new Writer("temporalFile.dat");
+        String id;
+
+        String line = lectorDelete.readLine();//Encabezado
+        //Pasamos a la siguiente línea
+        line = lectorDelete.readLine();
+
+        String linea = lector.readLine();
+
+        //Escribimos el encabezado
+        escritor.println(linea);
+
+        //Seguimos con las siguientes líneas
+        linea = lector.readLine();
+
+        while (line != null || linea != null){
+
+            id = line;
+
+            boolean salir = false;
+            boolean eliminado = false;
+
+            while(salir == false){
+
+                String campos[] = linea.split(":");
+
+                if (campos[0].equals(id)){
+                    
+                    eliminado = true;
+
+                } else {
+                    escritor.println(linea);
+                    //System.out.println(linea);
+                }
+                
+            
+                linea = lector.readLine();
+
+                if (eliminado || linea == null){
+                    salir = true;
+                }
+
+
+            }
+            
+            line = lectorDelete.readLine();
+        }
+
+        escritor.close();
+        lector.close();
+        lectorDelete.close();
+
+        //Eliminamos master.dat
+        Files.delete(this.masterFile);
+
+        //Renombramos el archivo temporal
+        Files.rename("temporalFile.dat", "master.dat");
+        this.masterFile = "master.dat";
 
     }
 
     public void update(){
         
+        Reader lectorUpdate = new Reader("update.dat");
+        Reader lector = new Reader(this.masterFile);
+        Writer escritor = new Writer("temporalFile.dat");
+        String id;
+
+        String line = lectorUpdate.readLine();//Encabezado
+        //Pasamos a la siguiente línea
+        line = lectorUpdate.readLine();
+
+        String linea = lector.readLine();
+
+        //Escribimos el encabezado
+        escritor.println(linea);
+
+        //Seguimos con las siguientes líneas
+        linea = lector.readLine();
+
+        while (line != null || linea != null){
+
+            if (line == null){
+                
+                id = null;
+                
+            } else {
+                
+                String fields [] = line.split(":");
+
+                id = fields[0];
+            }
+
+            boolean salir = false;
+            boolean actualizado = false;
+
+            while(salir == false){
+
+                String campos[] = linea.split(":");
+
+                if (campos[0].equals(id)){
+                    
+                    escritor.println(line);
+                    actualizado = true;
+
+                } else {
+                    escritor.println(linea);
+                    //System.out.println(linea);
+                }
+                
+            
+                linea = lector.readLine();
+
+                if (actualizado || linea == null){
+                    salir = true;
+                }
+
+            }
+            
+            line = lectorUpdate.readLine();
+        }
+
+        escritor.close();
+        lector.close();
+        lectorUpdate.close();
+
+        //Eliminamos master.dat
+        Files.delete(this.masterFile);
+
+        //Renombramos el archivo temporal
+        Files.rename("temporalFile.dat", "master.dat");
+        this.masterFile = "master.dat";
+
     }
 
 }
--- m03/archivos/practica1/crud/Program.java
+++ m03/archivos/practica1/crud/Program.java
@@ -3,14 +3,10 @@ public class Program {
     public static void main(String[] args){
 
         Crud c = new Crud("master.dat", "create.dat", "delete.dat", "update.dat", ":");
-        /*
-        Orden de ejecución de métodos
+        
         c.create();
         c.delete();
-        c.update();*/
-
-        c.create();
-
+        c.update();
 
     }
 }
