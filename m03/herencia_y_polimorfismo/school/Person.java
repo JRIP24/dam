93db28f2e4408314c1aac24515bc0264512f7a15
Ronald Intriago <ronaldintriago52@gmail.com>
2020-05-26 17:22:27 +0200

    update 2020/05/26-17:22

---
+++ m03/herencia_y_polimorfismo/school/Person.java
@@ -0,0 +1,26 @@
+public class Person{
+
+    private String name;
+    private String address;
+
+    public Person(){
+
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String n){
+        this.name = n;
+    }
+
+    public String getAddress(){
+        return this.address;
+    }
+
+    public void setAddress(String a){
+        this.address = a;
+    }
+
+}
---
+++ m03/herencia_y_polimorfismo/school/Student.java
@@ -0,0 +1,9 @@
+public class Student extends Person {
+    
+    private String cycle;
+    private int course;
+
+    public Student(){
+        super(name, address);
+    }
+}
