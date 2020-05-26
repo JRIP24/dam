db506af9e99ec2bc9027fd34302fdf64859e256f
Ronald Intriago <ronaldintriago52@gmail.com>
2020-05-26 20:16:38 +0200

    update 2020/05/26-20:16

--- m03/herencia_y_polimorfismo/school/Program.java
+++ m03/herencia_y_polimorfismo/school/Program.java
@@ -2,8 +2,15 @@ public class Program {
     
     public static void main(String[] args){
         
-        Person persona = new Person("Ronald", "Tordera 22");
+        Person persona = new Person("Ronald", "Dirección 22");
+        Teacher profe = new Teacher("Sue", "Calle 11");
         Student estudiante = new Student("Jhon", "Calle 22", "IT", 1);
         InternationalStudent gringo = new InternationalStudent("Jim", "Street 22", "CC", 1, "EEUU");
+
+        System.out.println("-----------------------");
+        System.out.println("Nombre de Persona: " + persona.getName());
+        System.out.println("Nombre de Teacher: " + profe.getName());
+        System.out.println("Nombre de Student: " + estudiante.getName());
+        System.out.println("Nombre de InternationalStudent: " + gringo.getName());
     }
 }
