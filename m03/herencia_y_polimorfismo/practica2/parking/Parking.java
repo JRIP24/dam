a8d7cba6e9ffd1b1f3d4bc49848a64bc628184d7
Ronald Intriago <ronaldintriago52@gmail.com>
2020-05-30 22:00:01 +0200

    update 2020/05/30-22:00

---
+++ m03/herencia_y_polimorfismo/practica2/parking/Parking.java
@@ -0,0 +1,32 @@
+public class Parking {
+    
+    private int nSpaces;
+    private Vehicle[] spaces;
+
+    public Parking(int n){
+        
+        this.nSpaces = n;
+        this.spaces = new Vehicle[this.nSpaces];
+    }
+
+    public boolean add(Vehicle v, int ns){
+
+    }
+
+    public Vehicle remove(int ns){
+
+    }
+
+    public double calculateFee(int ns){
+
+    }
+
+    public double calculateTotal(){
+
+    }
+
+    public void reportTotal(String pathReport){
+        
+    }
+
+}
---
+++ m03/herencia_y_polimorfismo/practica2/parking/Vehicle.java
@@ -0,0 +1,18 @@
+public abstract class Vehicle{
+
+    private String regPlate;
+    protected int power;
+
+    public Vehicle(String reg, int p){
+        
+        this.regPlate = reg;
+        this.power = p;
+    }
+
+    public abstract double calculateFee();
+    
+    public String getRegPlate(){
+
+        return this.regPlate;
+    }
+}
