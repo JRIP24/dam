9aa759b88f71ecb152cf7dfa3a3ec3557708f885
iam2671090 <iam2671090@i02.informatica.escoladeltreball.org>
2020-09-30 20:15:04 +0200

    update 2020/09/30-20:15

---
+++ segundo/m06/Actividad1_Clase_FILE_Java/prova2/src/prova2/Main.java
@@ -0,0 +1,26 @@
+package prova2;
+
+import java.io.File;
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String ruta;
+		Scanner teclado = new Scanner(System.in);
+		/*File dir = new File(ruta);
+		System.out.ptintln("Contenido del directorio");
+		System.out.println(dir.getAbsolutePath());
+		for (String c : dir.list())
+		System.out.println(c);*/
+		
+		System.out.print("Introduzca la RUTA ABSOLUTA del archivo que desea eliminar: ");
+		ruta = teclado.nextLine();
+		
+		
+		
+	}
+
+}
