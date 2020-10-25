e44685940090bb6a453b9c527be2211895d12e63
Ronald <ronaldintriago52@gmail.com>
2020-10-25 22:00:23 +0100

    update 2020/10/25-22:00

--- segundo/m08/AndroidStudio/Prueba/app/src/main/java/com/example/prueba/MainActivity.java
+++ segundo/m08/AndroidStudio/Prueba/app/src/main/java/com/example/prueba/MainActivity.java
@@ -1,14 +1,94 @@
 package com.example.prueba;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private CardView card1;
+    private CardView card2;
+    private CardView card3;
+    private CardView card4;
+    private CardView card5;
+    private CardView card6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Hook
+        card1 = (CardView) findViewById(R.id.card1);
+        card2 = (CardView) findViewById(R.id.card2);
+        card3 = (CardView) findViewById(R.id.card3);
+        card4 = (CardView) findViewById(R.id.card4);
+        card5 = (CardView) findViewById(R.id.card5);
+        card6 = (CardView) findViewById(R.id.card6);
+
+
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, CardView1Activity.class);
+
+                startActivity(intent);
+            }
+        });
+
+
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, CardView2Activity.class);
+
+                startActivity(intent);
+            }
+        });
+
+
+        card3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, CardView3Activity.class);
+
+                startActivity(intent);
+            }
+        });
+
+
+        card4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, CardView4Activity.class);
+
+                startActivity(intent);
+            }
+        });
+
+
+
+        card5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, CardView5Activity.class);
+
+                startActivity(intent);
+            }
+        });
+
+
+
+        card6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, CardView6Activity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
