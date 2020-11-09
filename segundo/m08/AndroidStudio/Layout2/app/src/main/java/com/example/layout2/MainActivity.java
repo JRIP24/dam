e439535e785da7debb0dacf94c23cbb6fdde951e
Ronald <ronaldintriago52@gmail.com>
2020-11-09 19:17:57 +0100

    update 2020/11/09-19:17

--- segundo/m08/AndroidStudio/Layout2/app/src/main/java/com/example/layout2/MainActivity.java
+++ segundo/m08/AndroidStudio/Layout2/app/src/main/java/com/example/layout2/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.layout2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView titulo;
+    TextView subTitulo;
+    CardView card1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Hook
+        titulo = (TextView) findViewById(R.id.title);
+        subTitulo = (TextView) findViewById(R.id.subTitle);
+        card1 = (CardView) findViewById(R.id.card1);
+
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent (MainActivity.this, CardViewActivity.class);
+            }
+        });
+
+
+
+
+
     }
 }
