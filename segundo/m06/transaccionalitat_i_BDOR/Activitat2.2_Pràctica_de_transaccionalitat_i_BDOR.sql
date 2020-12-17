ba92faf1933cd7d46487d740e2988b446472106c
Ronald <ronaldintriago52@gmail.com>
2020-12-17 18:43:04 +0100

    update 2020/12/17-18:43

--- segundo/m06/transaccionalitat_i_BDOR/Activitat2.2_Pràctica_de_transaccionalitat_i_BDOR.sql
+++ segundo/m06/transaccionalitat_i_BDOR/Activitat2.2_Pràctica_de_transaccionalitat_i_BDOR.sql
@@ -11,26 +11,34 @@ CREATE TABLE resum_facturacio(
 
 --Exercici 1
 
-DROP PROCEDURE IF EXISTS crea_resum_facturacio(p_mes, p_any DATE);
+DROP PROCEDURE IF EXISTS crea_resum_facturacio();
+
 DELIMITER //
+CREATE PROCEDURE crea_resum_facturacio(p_mes INT, p_any INT)
+BEGIN
+DECLARE v_dni INT;
+DECLARE v_total_facturacion DOUBLE(40,2);
+DECLARE acaba INT DEFAULT FALSE; 
 
 DECLARE c_comanda CURSOR FOR SELECT SUM(preu_total), dni_client FROM comanda WHERE YEAR(data) = p_any AND MONTH(data) = p_mes GROUP BY dni_client;
 --Saber el precio total de compra de un cliente en una mes y año concretos:
 --select SUM(preu_total) from comanda WHERE YEAR(data) = 2020 AND MONTH(data) = 08 GROUP BY dni_client;
 
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET acaba = TRUE; 
+
 OPEN c_comanda; 
-read_loop: LOOP 
-    FETCH c_comanda INTO v_dni, v_data_naixement;
-    IF acaba THEN
-        LEAVE read_loop;
-    END IF;
-    IF v_data_naixement > CURRENT_DATE() - INTERVAL 18 YEAR THEN
-        SET v_menor = 'S';
-    ELSE
-        SET v_menor = 'N';
-    END IF;
-    UPDATE persona SET menor = v_menor WHERE dni = v_dni;
+read_loop: LOOP
+    
+    FETCH c_comanda INTO v_total_facturacion, v_dni;
+
+    INSERT INTO resum_facturacio (mes, any, dni_client, quantitat ) 
+    VALUES (p_mes, p_any, v_dni, v_total_facturacion);
+
 END LOOP;
 CLOSE c_comanda;
 
-DELIMITER ;
+END;
+
+//
+
+CALL crea_resum_facturacio();
