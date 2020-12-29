d7f5807e2da5d2d2b709d21de50af0c3663c2f6d
Ronald <ronaldintriago52@gmail.com>
2020-12-29 16:33:59 +0100

    update 2020/12/29-16:33

---
+++ segundo/m08/AndroidStudio/JuegoCartas/app/src/main/java/com/example/juegocartas/SinglePlayerActivity.java
@@ -0,0 +1,14 @@
+package com.example.juegocartas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SinglePlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_player);
+    }
+}
