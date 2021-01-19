4410579257fc16a95468caa64a77dd729070ce23
Ronald <ronaldintriago52@gmail.com>
2021-01-19 19:03:49 +0100

    update 2021/01/19-19:03

---
+++ segundo/m05/Examen/test/veterinary/Visit.java
@@ -0,0 +1,19 @@
+package veterinary;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Visit {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
