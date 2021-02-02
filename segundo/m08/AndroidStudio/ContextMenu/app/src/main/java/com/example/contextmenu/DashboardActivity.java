23470ac7717f37c2d3f028b8cefbc46f3a3e9c62
Ronald <ronaldintriago52@gmail.com>
2021-02-02 17:52:49 +0100

    update 2021/02/02-17:52

--- segundo/m08/AndroidStudio/ContextMenu/app/src/main/java/com/example/contextmenu/DashboardActivity.java
+++ segundo/m08/AndroidStudio/ContextMenu/app/src/main/java/com/example/contextmenu/DashboardActivity.java
@@ -2,13 +2,30 @@ package com.example.contextmenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private Button listViewBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        //hook
+        listViewBtn = (Button) findViewById(R.id.listViewBtn);
+
+        listViewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (DashboardActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
