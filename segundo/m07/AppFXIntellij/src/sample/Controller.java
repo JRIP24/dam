80035d8764f1c79d3d210ebba3116189abe75498
Ronald <ronaldintriago52@gmail.com>
2021-03-03 15:48:04 +0100

    update 2021/03/03-15:48

--- segundo/m07/AppFXIntellij/src/sample/Controller.java
+++ segundo/m07/AppFXIntellij/src/sample/Controller.java
@@ -1,16 +1,29 @@
 package sample;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
+
+
+    private final StringProperty nombre = new SimpleStringProperty("nombre");
+    private final StringProperty nomFiscalText = new SimpleStringProperty("nomFiscal");
+    private final StringProperty direccionText = new SimpleStringProperty("");
+    private final StringProperty ciudadText = new SimpleStringProperty("");
+    private final StringProperty provinciaText = new SimpleStringProperty("");
 
     @FXML
     private Button btnDelete;
@@ -19,16 +32,55 @@ public class Controller {
     private Button btnServeis;
 
     @FXML
+    private Button btnConsulta;
+
+    @FXML
     private TextField textNom;
+
     @FXML
     private TextField textNomFiscal;
 
+    @FXML
+    private TextField direccion;
+
+    @FXML
+    private TextField ciudad;
+
+    @FXML
+    private TextField provincia;
+
+
+    public void initialize() {
+
+        //Bind entre controles
+        textNom.textProperty()
+                .bindBidirectional(textNomFiscal.textProperty());
+
+        //Bind de controles y atributos de la clase
+        nombre.bind(textNom.textProperty());
+        nomFiscalText.bind(textNomFiscal.textProperty());
+        direccionText.bind(direccion.textProperty());
+        ciudadText.bind(ciudad.textProperty());
+        provinciaText.bind(provincia.textProperty());
+
+    }
 
     @FXML
     public void clickDeleteBtn(ActionEvent actionEvent) {
         textNom.setText("");
-        textNom.textProperty()
-                .bindBidirectional(textNomFiscal.textProperty());
+        direccion.setText("");
+        ciudad.setText("");
+        provincia.setText("");
+    }
+
+    @FXML
+    public void clickConsultaBtn(ActionEvent actionEvent) {//Mostramos por cosola los datos enlazados
+
+        System.out.println("Nombre Fiscal: " + nomFiscalText.get());
+        System.out.println("Nombre: " + nombre.get());
+        System.out.println("Dirección: " + direccionText.get());
+        System.out.println("Ciudad: " + ciudadText.get());
+        System.out.println("Provincia: " + provinciaText.get());
     }
 
     public void clickBtnServeis(ActionEvent actionEvent) throws IOException {
